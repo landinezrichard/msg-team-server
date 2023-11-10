9a74116c66e62835c794d918245a8b8f2f2fef63
Ricardo Landínez <ricardo.landinez@ariadnacg.com>
2023-11-10 09:11:17 -0500

    Feat: iniciando proyecto backend MEAN API

---
+++ app.js
@@ -0,0 +1,11 @@
+// Cargar modulos de node para crear el servidor
+const express = require('express');
+
+// Ejecutar express (http)
+let app = express();
+
+// Añadir prefijos a rutas
+
+
+// Exportar modulo (fichero actual)
+module.exports = app;
